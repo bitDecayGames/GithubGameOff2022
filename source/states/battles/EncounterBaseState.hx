fea422cdc112dbbb38f6b2a6f848543c03dce352
Logan <monday.hopscotch@gmail.com>
2022-11-04 23:40:48 -0600

    more lucidtext cleanup

--- source/states/battles/EncounterBaseState.hx
+++ source/states/battles/EncounterBaseState.hx
@@ -1,5 +1,6 @@
 package states.battles;
 
+import com.bitdecay.lucidtext.parse.TagLocation;
 import com.bitdecay.lucidtext.TypeOptions;
 import flixel.math.FlxRect;
 import flixel.FlxG;
@@ -9,6 +10,9 @@ import flixel.FlxSubState;
 import com.bitdecay.lucidtext.TypingGroup;
 
 class EncounterBaseState extends FlxSubState {
+
+	var textTest:TypingGroup;
+
 	public function new() {
 		super();
 
@@ -26,14 +30,20 @@ class EncounterBaseState extends FlxSubState {
 		// battleMenuBG.screenCenter(FlxAxes.X);
 		// add(battleMenuBG);
 
-		var textTest = new TypingGroup(
+		textTest = new TypingGroup(
 			FlxRect.get(FlxG.width * 0.1, FlxG.height * .7, FlxG.width * .8, FlxG.height * .25),
-			"Welcome to the thunder dome. A pot stands <bigger>before</bigger> you. It <wave height=2>gazes</wave> <speed mod=0.1>deeeeply</speed> into your soul, as if to ask you to join it. Do you yield, <shake dist=1><rainbow>MORTAL?</rainbow></shake>",
+			"Welcome to the thunder dome. A pot stands before you. It <wave height=2>gazes</wave> <speed mod=0.1>deeeeply</speed> into your soul, as if to ask you to join it. You hear a quiet whisper, \"Do you yield, <cb val=camred /><shake dist=1><rainbow>MORTAL?</rainbow></shake><pause/>\"<cb val=camgrey />",
 			new TypeOptions(AssetPaths.battleMenuSlice__png, [4, 4, 7, 8], 4, 10)
 		);
-		for (m in textTest.members) {
-			m.scrollFactor.set();
-		}
+		textTest.tagCallback = (tag:TagLocation) -> {
+			if (tag.tag == "cb") {
+				if (tag.parsedOptions.val == "camred") {
+					camera.bgColor = FlxColor.RED;
+				} else if (tag.parsedOptions.val == "camgrey") {
+					camera.bgColor = FlxColor.GRAY;
+				}
+			}
+		};
 		add(textTest);
 	}
 
