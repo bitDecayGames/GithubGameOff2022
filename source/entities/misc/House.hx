68188d7237b0c479e1a6691128fcc751558965bd
Logan <monday.hopscotch@gmail.com>
2022-11-19 23:01:37 -0700

    parse in first pass of cludd's exterior house

--- source/entities/misc/House.hx
+++ source/entities/misc/House.hx
@@ -10,17 +10,19 @@ class House extends FlxSprite {
 	public function new(data:Entity_House) {
 		// TODO: Put a filler for when a thing isn't found
 		var asset = AssetPaths.crappot__png;
+		var yOffset = 0;
 		switch(data.f_HouseID) {
 			case "Lonk":
 				asset = GlobalQuestState.HAS_COMPASS ? AssetPaths.lonkBurned__png : AssetPaths.lonk__png;
+				yOffset = 16;
+			case "house_cludd":
+				asset = AssetPaths.cludds__png;
 			default:
 		}
 		super(asset);
 		eData = data;
-		setPosition(data.pixelX - width/2, data.pixelY - height + 16);
-		height = height - 16;
-
-		// TODO: these numbers likely need to be adjusted per house once we get more of them
+		setPosition(data.pixelX - width/2, data.pixelY - height + yOffset);
+		height = height - yOffset;
 	}
 
 	public function getDoors():Array<Door> {
